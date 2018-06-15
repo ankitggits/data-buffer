4791867038bc4b90347ac5f4d2edc80320695ccd
ankit <ankit.singh@storebrand.no>
2018-06-15 23:35:40 +0200

    clean up

--- src/main/java/no/sample/buffer/BufferBuilder.java
+++ src/main/java/no/sample/buffer/BufferBuilder.java
@@ -1,6 +1,5 @@
 package no.sample.buffer;
 
-import rx.Observable;
 import rx.functions.Action1;
 import rx.subjects.PublishSubject;
 
@@ -9,10 +8,8 @@ import java.util.List;
 public class BufferBuilder {
 
     public static <T> BufferPublisher<T> build(Action1<List<T>> action, BufferStats<T> bufferStats){
-        BufferOperator<T> bufferUntilOperator = new BufferOperator<>(bufferStats);
         PublishSubject<T> subject = PublishSubject.create();
-        Observable<List<T>> lift = subject.lift(bufferUntilOperator);
-        lift.subscribe(action, Throwable::printStackTrace);
+        subject.lift(new BufferOperator<>(bufferStats)).subscribe(action, Throwable::printStackTrace);
         return new BufferPublisher<T>(subject);
     }
 }
